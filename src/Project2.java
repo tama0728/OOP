3ed20bda6613a7c89dfa3a32092fdb4e9b0bc2a3
Kim Dongyun <edw0728@gmail.com>
2023-11-14 19:07:34 +0900

    Week 10

--- src/Project2.java
+++ src/Project2.java
@@ -1,8 +1,7 @@
-package Project2;
-
+import java.util.Scanner;
 import static java.lang.System.exit;
 
-public class Microwave {
+class Microwave {
     private int watt;
     private int time;
     private Boolean power;
@@ -83,5 +82,48 @@ public class Microwave {
     public void off() {
         System.out.println("안녕히 계세요~ 전자레인지였습니다.");
         exit(0);
+    }
+}
+
+public class Project2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Microwave mw = new Microwave();
+        int time;
+        int watt;
+        int select;
+        while (true) {
+            System.out.print("\n1. 계란찜, 2. 라면끓이기, 3. 감자삶기, 4. 시간(초) 설정, 5. 소비 전력(W) 설정, 6. 조리 시작, 기타. 종료\n메뉴 선택> ");
+            select = sc.nextInt();
+            switch (select) {
+                case 1:
+                case 2:
+                case 3:
+                    mw.setting(select);
+                    break;
+                case 4:
+                    System.out.print("입력값 (1~900)> ");
+                    time = sc.nextInt();
+                    if (time < 1 || time > 900)
+                        System.out.println("유효한 요리 시간(초)이 설정되어야 합니다.");
+                    else
+                        mw.setTime(time);
+                    break;
+                case 5:
+                    System.out.print("입력값 (200, 500, 700, 1000 중 하나)> ");
+                    watt = sc.nextInt();
+                    if (watt == 200 || watt == 500 || watt == 700 || watt == 1000)
+                        mw.setWatt(watt);
+                    else
+                        System.out.println("소비 전력(W)은 200, 500, 700, 1000만 지원합니다.");
+                    break;
+                case 6:
+                    mw.start();
+                    break;
+                default:
+                    mw.off();
+                    break;
+            }
+        }
     }
 }
